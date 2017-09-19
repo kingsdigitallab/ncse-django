f7b06ddf92d2634a48c8350f9da6567c1ef25bbf
Miguel Vieira <jmvieira@gmail.com>
2017-09-19 12:05:11 +0100

    Added model inlines.

--- periodicals/admin.py
+++ periodicals/admin.py
@@ -3,20 +3,47 @@ from django.contrib import admin
 from .models import Article, Issue, Page, Publication
 
 
+class IssueInline(admin.TabularInline):
+    model = Issue
+
+    extra = 1
+    show_change_link = True
+
+
 @admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
+    inlines = [IssueInline]
+
     list_display = ['abbreviation', 'title']
     list_filter = ['abbreviation']
 
 
+class PageInline(admin.TabularInline):
+    model = Page
+
+    extra = 1
+    show_change_link = True
+
+
 @admin.register(Issue)
 class IssueAdmin(admin.ModelAdmin):
+    inlines = [PageInline]
+
     list_display = ['uid', 'publication', 'issue_date']
     list_filter = ['publication__abbreviation', 'issue_date']
 
 
+class ArticleInline(admin.TabularInline):
+    model = Article
+
+    extra = 1
+    show_change_link = True
+
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
+    inlines = [ArticleInline]
+
     list_display = ['issue', 'number']
 
 
