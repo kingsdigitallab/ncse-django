2b7467d0d81b8ef216870007793f8557b6e67fdc
Miguel Vieira <jmvieira@gmail.com>
2017-09-20 10:42:33 +0100

    Added haystack urls.

--- mpol/urls.py
+++ mpol/urls.py
@@ -1,16 +1,14 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-
-
 from kdl_ldap.signal_handlers import \
     register_signal_handlers as kdl_ldap_register_signal_hadlers
-
-kdl_ldap_register_signal_hadlers()
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailsearch.urls import frontend as wagtailsearch_frontend_urls
+
+kdl_ldap_register_signal_hadlers()
 
 
 admin.autodiscover()
@@ -19,11 +17,11 @@ urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^search/', include('haystack.urls')),
 
     url(r'^wagtail/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'^search/', include(wagtailsearch_frontend_urls)),
-    url(r'^search/', include('haystack.urls')),
     url(r'', include(wagtail_urls)),
 ]
 
