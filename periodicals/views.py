6913de7f8f9d9c04c2c7e02f044c62e17624ae0c
Brian Maher <brian.maher@kcl.ac.uk>
2017-10-06 11:27:22 +0000

    Moving vars out of the view class.

--- periodicals/views.py
+++ periodicals/views.py
@@ -10,32 +10,33 @@ class ArticleDetailView(DetailView):
 
 class PageDetailView(TemplateView):
     template_name = 'periodicals/page_detail.html'
-    page = None
-    highlight_words = {}
 
     def get_context_data(self, **kwargs):
         context = super(PageDetailView, self).get_context_data(**kwargs)
 
         # Get our page
         page_number = kwargs.get('pk', None)
-        self.page = Page.objects.get(pk=page_number)
+        page = Page.objects.get(pk=page_number)
+
+        # Words to highlight
+        highlight_words = {}
 
         # Check if we need to do any highlighting
         if 'highlight' in self.request.GET:
             # Get the words to highlight
             words_to_highlight = self.request.GET['highlight'].split()
-            page_words = self.page.words
+            page_words = page.words
 
             for word in words_to_highlight:
                 try:
-                    self.highlight_words.update({word: page_words[word]})
+                    highlight_words.update({word: page_words[word]})
                 except KeyError:
                     pass  # Not found
 
         # Set all of the things
-        if self.highlight_words:
-            context['highlight_words'] = self.highlight_words
-        context['page'] = self.page
+        if highlight_words:
+            context['highlight_words'] = highlight_words
+        context['page'] = page
         return context
 
 
