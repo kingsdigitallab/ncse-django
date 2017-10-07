b4fc510f3a61a53db02d69edf8e555a552491567
Miguel Vieira <jmvieira@gmail.com>
2017-10-07 15:57:22 +0100

    Removed fields that are no longer in the models and added an issue year field.

--- periodicals/search_indexes.py
+++ periodicals/search_indexes.py
@@ -9,21 +9,21 @@ class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     aid = indexes.CharField(model_attr='aid', indexed=False)
     title = indexes.CharField(model_attr='title')
     description = indexes.CharField(model_attr='description')
-    # text = indexes.CharField(document=True, model_attr='get_text')
     text = indexes.CharField(document=True, model_attr='content')
 
     page = indexes.IntegerField(model_attr='page__id', indexed=False)
     page_number = indexes.IntegerField(
         model_attr='page__number', indexed=False)
     page_image = indexes.CharField(model_attr='page__image', indexed=False)
-    page_pdf = indexes.CharField(model_attr='page__pdf', indexed=False)
 
     issue = indexes.IntegerField(model_attr='issue__id', indexed=False)
     issue_uid = indexes.CharField(model_attr='issue__uid', indexed=False)
     issue_date = indexes.FacetDateField(model_attr='issue__issue_date')
+    issue_year = indexes.FacetIntegerField(
+        model_attr='issue__issue_date__year')
     issue_number_of_pages = indexes.IntegerField(
         model_attr='issue__number_of_pages', indexed=False)
-    issue_pdf = indexes.CharField(model_attr='issue__pdf', indexed=False)
+    # issue_pdf = indexes.CharField(model_attr='issue__pdf', indexed=False)
 
     publication = indexes.IntegerField(
         model_attr='page__issue__publication__id', indexed=False)
