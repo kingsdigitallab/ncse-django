a02f0e10c1d9044448c50820308970db6f80469e
Miguel Vieira <jmvieira@gmail.com>
2017-10-08 14:47:36 +0100

    Implementing facetd search view (work in progress).

--- periodicals/forms.py
+++ periodicals/forms.py
@@ -1,17 +1,33 @@
 from django import forms
+from django.forms.widgets import RadioSelect
 from haystack.forms import FacetedSearchForm
 
 
 class PeriodicalsSearchForm(FacetedSearchForm):
+    MODE_DEFAULT = 'or'
+    MODE_AND = 'and'
+    MODE_PHRASE = 'phrase'
+    MODE_CHOICES = ((MODE_DEFAULT, 'Any word'),
+                    (MODE_AND, 'All the words'),
+                    (MODE_PHRASE, 'Phrase'))
+    mode = forms.ChoiceField(choices=MODE_CHOICES,
+                             initial=MODE_DEFAULT, widget=RadioSelect)
     start_year = forms.IntegerField(required=False)
     end_year = forms.IntegerField(required=False)
 
     def search(self):
-        # First, store the SearchQuerySet received from other processing.
         sqs = super(PeriodicalsSearchForm, self).search()
 
         if not self.is_valid():
             return self.no_query_found()
+
+        if self.cleaned_data['mode'] and self.cleaned_data['q']:
+            mode = self.cleaned_data['mode']
+
+            if mode == self.MODE_AND:
+                sqs = sqs.filter_and(content=self.cleaned_data['q'])
+            elif mode == self.MODE_PHRASE:
+                sqs = sqs.filter(content__exact=self.cleaned_data['q'])
 
         if self.cleaned_data['start_year']:
             sqs = sqs.filter(issue_year__gte=self.cleaned_data['start_year'])
