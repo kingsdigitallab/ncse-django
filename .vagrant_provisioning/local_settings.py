ebff12792a37628c4f61ec26324c079de5c9fe95
Brian Maher <brian.maher@kcl.ac.uk>
2017-10-12 14:37:00 +0000

    Updated local settings in vagrant provisioning

--- .vagrant_provisioning/local_settings.py
+++ .vagrant_provisioning/local_settings.py
@@ -37,3 +37,18 @@ try:
     DEBUG_TOOLBAR_PATCH_SETTINGS = True
 except ImportError:
     pass
+
+# Change as required
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+        'URL': 'http://localhost:8983/solr/dev',
+        'TIMEOUT': 60 * 5,
+        'INCLUDE_SPELLING': True,
+        'BATCH_SIZE': 100,
+    },
+}
+
+LOGGING['loggers']['periodicals'] = {}
+LOGGING['loggers']['periodicals']['handlers'] = ['console']
+LOGGING['loggers']['periodicals']['level'] = logging.DEBUG
