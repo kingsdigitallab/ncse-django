14bee264ab5289bee8b34ad25178d5719a5c9c2d
Brian Maher <brian.maher@kcl.ac.uk>
2017-10-27 12:53:24 +0000

    Updated search index to allow null values

--- periodicals/search_indexes.py
+++ periodicals/search_indexes.py
@@ -6,41 +6,43 @@ from .models import Article
 class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
     url = indexes.CharField(model_attr='url', indexed=False)
     aid = indexes.CharField(model_attr='aid', indexed=False)
-    title = indexes.CharField(model_attr='title')
-    description = indexes.CharField(model_attr='description')
-    category = indexes.FacetCharField(model_attr='article_type')
+    title = indexes.CharField(model_attr='title', null=True)
+    description = indexes.CharField(model_attr='description', null=True)
+    category = indexes.FacetCharField(model_attr='article_type', null=True)
     image = indexes.CharField(model_attr='title_image', indexed=False)
-    text = indexes.CharField(document=True, model_attr='content')
+    text = indexes.CharField(document=True, model_attr='content', null=True)
     slug = indexes.CharField(model_attr='slug', indexed=False)
     page = indexes.IntegerField(model_attr='page__id', indexed=False)
-    position_in_page = indexes.IntegerField(model_attr='position_in_page')
-    page_url = indexes.CharField(indexed=False)
+    position_in_page = indexes.IntegerField(
+        model_attr='position_in_page', null=True)
+    page_url = indexes.CharField(indexed=False, null=True)
     page_number = indexes.IntegerField(
         model_attr='page__number', indexed=False)
     page_image = indexes.CharField(model_attr='page__image', indexed=False)
 
     issue = indexes.IntegerField(model_attr='issue__id', indexed=False)
-    issue_url = indexes.CharField(indexed=False)
+    issue_url = indexes.CharField(indexed=False, null=True)
     issue_uid = indexes.CharField(model_attr='issue__uid', indexed=False)
     issue_slug = indexes.CharField(model_attr='issue__slug', indexed=False)
-    issue_date = indexes.FacetDateField(model_attr='issue__issue_date')
+    issue_date = indexes.FacetDateField(
+        model_attr='issue__issue_date', null=True)
     issue_year = indexes.FacetIntegerField(
-        model_attr='issue__issue_date__year')
+        model_attr='issue__issue_date__year', null=True)
     issue_number_of_pages = indexes.IntegerField(
-        model_attr='issue__number_of_pages', indexed=False)
+        model_attr='issue__number_of_pages', indexed=False, null=True)
     issue_pdf = indexes.CharField(model_attr='issue__pdf', indexed=False)
 
     publication = indexes.IntegerField(
         model_attr='issue__publication__id', indexed=False)
-    publication_url = indexes.CharField(indexed=False)
+    publication_url = indexes.CharField(indexed=False, null=True)
     publication_abbreviation = indexes.FacetCharField(
-        model_attr='issue__publication__abbreviation')
+        model_attr='issue__publication__abbreviation', null=True)
     publication_slug = indexes.CharField(
         model_attr='issue__publication__slug', indexed=False)
     publication_title = indexes.FacetCharField(
-        model_attr='issue__publication__title')
+        model_attr='issue__publication__title', null=True)
     publication_description = indexes.CharField(
-        model_attr='issue__publication__description')
+        model_attr='issue__publication__description', null=True)
 
     def get_model(self):
         return Article
