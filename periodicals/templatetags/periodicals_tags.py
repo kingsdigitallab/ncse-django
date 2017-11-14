0bd04b698b92bec7b8ebe969087f7b3a4f031ecd
Elliott Hall <elliott.hall@kcl.ac.uk>
2017-11-14 17:20:28 +0000

    Added pagination to search and get_request_parameters

--- periodicals/templatetags/periodicals_tags.py
+++ periodicals/templatetags/periodicals_tags.py
@@ -17,3 +17,17 @@ def initial_search_parameters():
             params = '{}{}'.format(params, value.initial)
 
     return params
+
+
+# Borrowed from wagtailbase
+@register.assignment_tag(takes_context=True)
+def get_request_parameters(context, exclude=None):
+    """Returns a string with all the request parameters except the exclude
+    parameter."""
+    params = ''
+    request = context['request']
+
+    for key, value in request.GET.items():
+        if key != exclude:
+            params += '&{key}={value}'.format(key=key, value=value)
+    return params
