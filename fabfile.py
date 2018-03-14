39c72da0d2ada3f209fd29136cdfe841ed48445a
Miguel Vieira <jmvieira@gmail.com>
2018-03-14 12:12:57 +0000

    Squashed commits: - Added initial data file for the db - Fixed server setting in fabfile

--- fabfile.py
+++ fabfile.py
@@ -33,11 +33,11 @@ REPOSITORY = 'https://github.com/kingsdigitallab/{}-django.git'.format(
 
 env.gateway = 'ssh.kdl.kcl.ac.uk'
 # Host names used as deployment targets
-env.hosts = ['{}.kdl.kcl.ac.uk'.format(PROJECT_NAME)]
+env.hosts = ['{}2.kdl.kcl.ac.uk'.format(PROJECT_NAME)]
 # Absolute filesystem path to project 'webroot'
-env.root_path = '/vol/ncse/webroot/'
+env.root_path = '/vol/ncse2/webroot/'
 # Absolute filesystem path to project Django root
-env.django_root_path = '/vol/ncse/webroot/'
+env.django_root_path = '/vol/ncse2/webroot/'
 # Absolute filesystem path to Python virtualenv for this project
 env.envs_path = os.path.join(env.root_path, 'envs')
 # -------------------------------
