c36d5f3bba1422c07f3d1c95715d4f81b77e9a3e
Brian Maher <brian.maher@kcl.ac.uk>
2018-05-01 10:30:29 +0000

    Canvas fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,104 +4,68 @@ var word_highlight_colour = 'rgba(225,225,0,0.2)'
 var article_bounding_box_colour = 'rgba(104,152,204,1)'
 
 
+
 // Article bounding box colour
 // Enables drawing to a canvas
 function enableCanvas() {
-
-    var scale_up = 1.25;
-    var scale_down = 0.8;
-    var zoom_level = 1;
-    var min_scale = 1;
-    var max_scale = 4;
-
-
-    // Redraws our canvas
-    function redraw(canvas)
-    {
-        var ctx = canvas.getContext('2d')
-            var img = new Image()
+    // Only run if we have a page canvas!
+    if ($(".pageCanvas").length) {
+        $(".pageCanvas").each(function() {
+            var canvas = $(this)[0];
+            var ctx = canvas.getContext("2d");
+            var img = new Image();
             img.onload = function() {
-                ctx.drawImage(this, 0, 0)
-                if (typeof highlight_words !== 'undefined') {
-                    ctx.lineWidth = '1'
-                    ctx.fillStyle = word_highlight_colour
-                    ctx.strokeStyle = 'rgba(0,0,0,0)'
+                ctx.drawImage(this, 0, 0);
+                if (typeof highlight_words !== "undefined") {
+                    ctx.lineWidth = "1";
+                    ctx.fillStyle = word_highlight_colour;
+                    ctx.strokeStyle = "rgba(0,0,0,0)";
 
                     jQuery.each(highlight_words, function(k1, v1) {
                         jQuery.each(v1, function(k2, v2) {
-                            var x0 = parseInt(this['x0'])
-                            var x1 = parseInt(this['x1'])
+                            var x0 = parseInt(this["x0"]);
+                            var x1 = parseInt(this["x1"]);
 
-                            var y0 = parseInt(this['y0'])
-                            var y1 = parseInt(this['y1'])
-                            ctx.beginPath()
-                            ctx.fillRect(x0, y0, x1 - x0, y1 - y0)
-                            ctx.stroke()
-                        })
-                    })
+                            var y0 = parseInt(this["y0"]);
+                            var y1 = parseInt(this["y1"]);
+                            ctx.beginPath();
+                            ctx.fillRect(x0, y0, x1 - x0, y1 - y0);
+                            ctx.stroke();
+                        });
+                    });
                 }
 
-                // IE Issues Start Here
-                if (typeof article_bounding_box !== 'undefined') {
-                    ctx.lineWidth = '4'
-                    ctx.fillStyle = 'rgba(0,0,0,0)'
-                    ctx.strokeStyle = article_bounding_box_colour
+// IE Issues Start Here
+                if (typeof article_bounding_box !== "undefined") {
+                    ctx.lineWidth = "4";
+                    ctx.fillStyle = "rgba(0,0,0,0)";
+                    ctx.strokeStyle = article_bounding_box_colour;
 
-                    ctx.beginPath()
+                    ctx.beginPath();
                     ctx.moveTo(
-                        parseInt(article_bounding_box[0]['x']),
-                        parseInt(article_bounding_box[0]['y'])
-                    )
+                        parseInt(article_bounding_box[0]["x"]),
+                        parseInt(article_bounding_box[0]["y"])
+                    );
 
-                    count = article_bounding_box.length
+                    count = article_bounding_box.length;
 
                     for (var i = 1; i <= count; i++) {
                         ctx.lineTo(
-                            parseInt(article_bounding_box[i % count]['x']),
-                            parseInt(article_bounding_box[i % count]['y'])
-                        )
+                            parseInt(article_bounding_box[i % count]["x"]),
+                            parseInt(article_bounding_box[i % count]["y"])
+                        );
                     }
 
-                    ctx.stroke()
+                    ctx.stroke();
                 }
-            }
-            img.src = $('#' + $(canvas).attr('data-image-id')).attr('src');
-    }
-
-
-    // Only run if we have a page canvas!
-    if ($('.pageCanvas').length) {
-        $('.pageCanvas').each(function() {
-            var canvas = $(this)[0]
-            redraw(canvas);
+            };
+            img.src = $("#" + $(this).attr("data-image-id")).attr("src");
         });
 
-        // IE Issues End Here
+// IE Issues End Here
     }
-
-    $('body').on('click', '.canvas-controls a[data-zoom]', function(event)
-    {
-        event.preventDefault();
-        event.stopPropagation();
-
-        var canvas = $('#' + $(this).attr('data-for'))[0];
-        var ctx = canvas.getContext('2d')
-        
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        // Adjust our scale
-        if($(this).attr('data-zoom') == 'in')
-        {
-            ctx.scale(scale_up,scale_up);   
-        } else
-        {
-            ctx.scale(scale_down,scale_down);   
-        }
-
-        redraw(canvas);
-
-    });
 }
+
 
 function enableReadMore() {
     $('body').on('click', '.read-more', function(event) {
@@ -118,8 +82,8 @@ function enableReadMore() {
 function enablePublicationSwitcher() {
     if ($('#publication-year-switcher').length) {
         $('body').on('click', '#publication-year-switcher a', function(event) {
-            event.preventDefault()
-            event.stopPropagation()
+            event.preventDefault();
+            event.stopPropagation();
 
             $.get($(this).attr('href'), function(data) {
                 $('#issues-ajax').html(data)
@@ -138,12 +102,28 @@ function enablePublicationSwitcher() {
     }
 }
 
+function resizeViewer()
+{
+     $("body").on("click", "#viewer-right a", function()
+    {
+        if(!$('[aria-expanded="true"]').length)
+        {
+            $('#viewer-left').addClass('medium-9').removeClass('medium-6');
+            $('#viewer-right').addClass('medium-3').removeClass('medium-6');
+        } else
+        {
+            $('#viewer-left').removeClass('medium-9').addClass('medium-6');
+            $('#viewer-right').removeClass('medium-3').addClass('medium-6');
+        }
+    });
+}
 $(function() {
-    if ($('canvas')) {
+    if ($('canvas').length) {
         enableCanvas()
     }
 
-    enablePublicationSwitcher()
+    enablePublicationSwitcher();
+    resizeViewer();
 
     if ($('#jump-to-results-section').size > 0) {
         // Jump to results if search has been run
