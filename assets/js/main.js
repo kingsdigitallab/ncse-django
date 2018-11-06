3adbcfba0510c176c96fa66a73329c870d9780e8
Brian Maher <brian.maher@kcl.ac.uk>
2018-11-06 12:58:04 +0000

    CSS changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -444,7 +444,8 @@ function enableVis()
                     tooltip.transition()
                       .duration(200)
                       .style("opacity", 1);
-                    tooltip.html("<span>" + d.y + " items" + "</span>")
+                      console.log(d3.event);
+                    tooltip.html("<span>" + d.y  + "</span>")
                       .style("left", (d3.event.pageX - 25) + "px")
                       .style("top", (d3.event.pageY - 28) + "px");
                   })
